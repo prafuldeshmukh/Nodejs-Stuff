d4aa66796798c2b2b68d60c348d0ca1c9ae3cf0b
prafuldeshmukh <prafull.deshmukh90@gmail.com>
2024-03-07 05:05:00 +0530

    done small changes

--- javascript/Promises/index.js
+++ javascript/Promises/index.js
@@ -4,14 +4,14 @@ What is promise in javascript ?
 => promises object is a placeholder for a certain period of time until we receive a value from 
 an asynchronous function 
 => a Container for future value
-=> A promise is an object that representing the eventual complation or faliure of asynchronous operation
+=> A promise is an object that representing the eventual complation or faliure of asynchronous operation, it's a returned object to which you attach callbacks, instead of passing callbacks into a function.
 => We were passing a function inside another API and we were given control of our program, into some other API Which we want to avoid that was inveraion of control
 => How do we solve it we basically had a promsie object and that promise object will be evetually 
    filled with a result of async operation that promise object
  => We don't pass a function  inside any other function but what we do it we attach a callacbk function to that promise object Whever we have data inside or whenever the promise is resolved 
  autmatically that callback function will be called by promises 
  => Promises give us  guarantee & trust in our whole transactions 
- => How we can resolve th promises jsut once and u can  only have three states for promises
+ => How we can resolve the promises just once and u can  only have three states for promises
     Pending, fulfilled & Rejected 
  => promise is immutable , U can't alter a promise, we can pass promises without care
  => So promise comes with a lot of trust , It was a special object Which had two major properties 
--- javascript/callback/index.js
+++ javascript/callback/index.js
@@ -1,9 +1,11 @@
 /*
   Callback Hell & Inversion Of Control
+  => Callbacks are the traditional approach to handle asynchronous operations in JavaScript. A callback is a function passed as an argument to another function and is executed once the asynchronous operation completes. Callbacks can result in deeply nested code structures, also known as callback hell, making the code harder to read and maintain.
+  => By definition, a callback is a function that you pass into another function as an argument for executing later.
   => Javascript is a synchronous single thread language and it can do just one thing at a time
   => It has one call satck & it can execute just one thing at a time 
   => Whatever code will give to javascript it will quickly execute by javascript engine , Does n't wait for anything
-  => What if we really need to wait for something , U want toexecute some API after 5 seconds
+  => What if we really need to wait for something , U want to execute some API after 5 seconds
      how will do that , using callback
   =>  Callback is a powerful way to do asynchronous thing in javascript, so we can take a code 
       piece of code inside a function and we can just pass it as a callback which can be executed 
@@ -36,8 +38,7 @@
              function now we have given control of this function to some other function
              We don't know whether that function will ever execute or callback  
           => It is another problem we see while using callbacks 
-          => Invserion of control is like that u lose the control of your codd when we are using callback
-      => create order an important piece of our code , isn't it proceed to payment
+          => Invserion of control is like that u lose the control of your code when we are using callback
       => We gave the control of program to create order API 
       => Now it is the resposibility of create order API to call our function back 
          and we are blindy trusting create order API , We don't know create order API 
@@ -45,7 +46,7 @@
       => What if our callback function is never called can happen 
       => what if our callback function is called twice
       => Whenever we have this callback function and pass it to some other function 
-      => We are giving the control of our function & our piece of written code to some other code 
+      => We are giving the control of our function / our piece of written code to some other code 
       => We don't know what's happening behind the scenses now 
      
       
@@ -56,6 +57,95 @@
                     api.showOrderSummary();
                })
        })
+
+
+       What are Callbacks?
+
+A callback is a function that is passed as an argument to another function, and is called after the main function has finished its execution.
+Callbacks allow you to handle the results of an asynchronous operation in a non-blocking manner, which means that the program can continue to run while the operation is being executed.
+
+Why use Callbacks?
+
+Callbacks are used to handle the results of asynchronous operations in a non-blocking manner. Asynchronous operations are operations that take a significant amount of time to complete, such as network requests, file I/O, and database queries. If these operations were executed synchronously, the program would freeze and wait for the operation to complete before continuing. This can lead to a poor user experience, as the program would appear unresponsive.
+
+Callbacks allow you to continue executing code while the operation is being executed in the background. Once the operation has completed, the callback function is called with the result of the operation. This way, you can ensure that the program remains responsive and the user experience is not impacted.
+
+
+
+
+Suppose that you need to develop a script that downloads a picture from a remote server and processes it after the download completes:
+
+function download(url) {
+    // ...
+}
+
+function process(picture) {
+    // ...
+}
+
+download(url);
+process(picture);
+Code language: JavaScript (javascript)
+However, downloading a picture from a remote server takes time depending on the network speed and the size of the picture.
+
+The following download() function uses the setTimeout() function to simulate the network request:
+
+function download(url) {
+    setTimeout(() => {
+        // script to download the picture here
+        console.log(`Downloading ${url} ...`);
+    },1000);
+}
+Code language: JavaScript (javascript)
+And this code emulates the process() function:
+
+function process(picture) {
+    console.log(`Processing ${picture}`);
+}
+Code language: JavaScript (javascript)
+When you execute the following code:
+
+let url = 'https://www.javascripttutorial.net/pic.jpg';
+
+download(url);
+process(url);
+Code language: JavaScript (javascript)
+you will get the following output:
+
+Processing https://javascripttutorial.net/pic.jpg
+Downloading https://javascripttutorial.net/pic.jpg ...
+Code language: JavaScript (javascript)
+This is not what you expected because the process() function executes before the download() function. The correct sequence should be:
+
+Download the picture and wait for the download complete.
+Process the picture.
+To resolve this issue, you can pass the process() function to the download() function and execute the process() function inside the download() function once the download completes, like this:
+
+function download(url, callback) {
+    setTimeout(() => {
+        // script to download the picture here
+        console.log(`Downloading ${url} ...`);
+        
+        // process the picture once it is completed
+        callback(url);
+    }, 1000);
+}
+
+function process(picture) {
+    console.log(`Processing ${picture}`);
+}
+
+let url = 'https://wwww.javascripttutorial.net/pic.jpg';
+download(url, process);
+Code language: JavaScript (javascript)
+Output:
+
+Downloading https://www.javascripttutorial.net/pic.jpg ...
+Processing https://www.javascripttutorial.net/pic.jpg
+Code language: JavaScript (javascript)
+Now, it works as expected.
+
+In this example, the process() is a callback passed into an asynchronous function.
 
        
 
